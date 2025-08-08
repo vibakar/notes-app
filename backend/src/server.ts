14e363ef28f443fbea101712587bb9684f5ea189
vibakar <vibakar23@gmail.com>
2025-08-08 19:41:36 +0100

    configured 0.0.0.0 host for backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,8 +3,9 @@ import app from "./app";
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  const HOST = "0.0.0.0";
+  app.listen(PORT, HOST, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 };
 
